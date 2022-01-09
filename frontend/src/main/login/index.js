bab6ed3db10e1215dc03ce26d4a2fb84df4b951d
Jay Herron <NeedleInAJayStack@protonmail.com>
2022-01-08 19:43:30 -0700

    Frontend: Changed login to function

--- frontend/src/main/login/index.js
+++ frontend/src/main/login/index.js
@@ -11,79 +11,68 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
+import TextField from '@mui/material/TextField';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: ''
-    };
-    this.handleSetUsername = this.handleSetUsername.bind(this);
-    this.handleSetPassword = this.handleSetPassword.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function Login(props) {
+  const [state, setState] = React.useState({
+    username: "",
+    password: "",
+  });
 
-  handleSetUsername(event) {
-    this.setState({username: event.target.value});
+  function handleSetUsername(event) {
+    setState({ ...state, username: event.target.value });
   }
-  handleSetPassword(event) {
-    this.setState({password: event.target.value});
+  function handleSetPassword(event) {
+    setState({ ...state, password: event.target.value });
   }
-  handleSubmit(event) {
+  function handleSubmit(event) {
+    event.preventDefault();
+
     fetch('http://localhost:8080/auth/token', {
       method: 'GET',
       headers: {
-        'Authorization': 'Basic ' + base64.encode(this.state.username + ":" + this.state.password)
+        'Authorization': 'Basic ' + base64.encode(state.username + ":" + state.password)
       }
     }).then(response => {
       if (response.ok) {
         response.json().then(result =>
-          this.props.onGetToken(result.token)
+          props.onGetToken(result.token)
         )
       }
     });
-
-    event.preventDefault();
   }
 
-  render() {
-    return (
-      <Box component="div" sx={{ display: 'flex', justifyContent: 'center', overflow: 'hidden'}}>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                JayHerron.org
-              </Typography>
-              <IconButton
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: -2 }}
-              >
-                <MenuIcon />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-        </Box>
-        <Box sx={{position: 'absolute', top: '50%', transform: 'translateY(-50%)', width: '100%', maxWidth: 400}}>
-          <Container>
-            <Stack spacing={2}>
-              <InputLabel htmlFor="username">Username:</InputLabel>
-              <Input id="username" type="text" value={this.state.username} onChange={this.handleSetUsername} />
-              <InputLabel htmlFor="password">Password:</InputLabel>
-              <Input id="password" type="password" value={this.state.password} onChange={this.handleSetPassword} />
-              <Button variant="contained" onClick={this.handleSubmit} >Log In</Button>
-            </Stack>
-          </Container>
-        </Box>
+  return (
+    <Box component="div" sx={{ display: 'flex', justifyContent: 'center', overflow: 'hidden'}}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              JayHerron.org
+            </Typography>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: -2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
       </Box>
-    );
-  }
+      <Box component="form" sx={{position: 'absolute', top: '50%', transform: 'translateY(-50%)', width: '100%', maxWidth: 400}}>
+        <Container>
+          <Stack spacing={2}>
+            <TextField id="username" label="Username" variant="outlined" onChange={handleSetUsername} />
+            <TextField id="password" type="password" label="Password" variant="outlined" onChange={handleSetPassword} />
+            <Button variant="contained" onClick={handleSubmit} >Log In</Button>
+          </Stack>
+        </Container>
+      </Box>
+    </Box>
+  );
 }
 
 export default Login;
