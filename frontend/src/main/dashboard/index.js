8cbaf8591d04033ca880b7d7948da1d45446ecb9
Jay Herron <NeedleInAJayStack@protonmail.com>
2022-01-11 22:48:49 -0700

    Frontend: Adds data query and point hydration

--- frontend/src/main/dashboard/index.js
+++ frontend/src/main/dashboard/index.js
@@ -1,94 +1,127 @@
 import React from "react";
 
-import formatISO from 'date-fns/formatISO';
+import getUnixTime from 'date-fns/getUnixTime';
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 
-export default function Dashboard(props) {
-	let token = props.token;
-	
-	const [state, setState] = React.useState({
-		point: "",
-		startDate: Date(),
-		endDate: Date(),
-		data: null
-	});
-	
-  function handleGetData(event) {
-    event.preventDefault();
-		let startDateStr = formatISO(state.startDate);
-		let endDateStr = formatISO(state.endDate);
-		let url = "http://localhost:8080/his/" + state.point + "?start=" + startDateStr + ",end=" + endDateStr;
-		console.log(url);
+export default class Dashboard extends React.Component {
+
+	constructor(props) {
+		super(props);
 		
-		fetch("http://localhost:8080/his/" + state.point + "?start=" + startDateStr + ",end=" + endDateStr, {
+		this.token = props.token;
+		this.state = {
+			isFetching: false,
+			points: [],
+			point: "",
+			startDate: Date.now(),
+			endDate: Date.now(),
+			his: null
+		};
+	}
+
+	componentDidMount() {
+		this.fetchPoints();
+		this.timer = setInterval(() => this.fetchPoints(), 5000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+		this.timer = null;
+	}
+
+  handlePointSelect(event) {
+    event.preventDefault();
+		this.setState({...this.state, point: event.target.value});
+	}
+
+	async fetchPoints() {
+		try {
+			let response = await fetch("http://localhost:8080/recs/", {
+			method: 'GET',
+				headers: {
+					'Authorization': 'Bearer ' + this.token
+				}
+			})
+			let points = await response.json();
+			this.setState({...this.state, points: points});
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	
+  async fetchHis(event) {
+    event.preventDefault();
+		this.setState({...this.state, isFetching: true});
+		let startDateSecs = getUnixTime(this.state.startDate);
+		let endDateSecs = getUnixTime(this.state.endDate);
+		
+		try {
+			let response = await fetch("http://localhost:8080/his/" + this.state.point + "?start=" + startDateSecs + "&end=" + endDateSecs, {
 				method: 'GET',
 				headers: {
-					'Authorization': 'Bearer ' + token
+					'Authorization': 'Bearer ' + this.token
 				}
-		}).then(response => {
-			if (response.ok) {
-				response.json().then(result => {
-					console.log(result);
-					setState({...state, data: result});
-				})
-			} else {
-				alert("User or password not recognized")
-			}
-		});
+			});
+			let his = await response.json();
+			console.log(his);
+			this.setState({...this.state, isFetching: false, his: his});
+		} catch (e) {
+			this.setState({...this.state, isFetching: false});
+			console.log(e);
+		}
 	}
 
-  function handlePointSelect(event) {
-    event.preventDefault();
-		setState({...state, point: event.target.value});
+	render() {
+		return (
+			<Stack direction="row" spacing={2} sx={{ display: "flex", marginTop: 5 }}>
+				<Box sx={{ minWidth: 120 }}>
+					<FormControl fullWidth>
+						<InputLabel id="point-select-label">Point</InputLabel>
+						<Select
+							labelId="point-select-label"
+							id="point-select"
+							value={this.state.point}
+							label="Point"
+							onChange={(event) => { this.handlePointSelect(event) }}
+						>
+						{this.state.points.map( point => <MenuItem key={point.id} value={point.id}>{point.dis}</MenuItem> )}
+						</Select>
+					</FormControl>
+				</Box>
+				<LocalizationProvider dateAdapter={AdapterDateFns}>
+					<DatePicker
+						label="Start date"
+						value={this.state.startDate}
+						onChange={(newDate) => {
+							this.setState({...this.state, startDate: newDate});
+						}}
+						renderInput={(params) => <TextField {...params} />}
+					/>
+					<DatePicker
+						label="End date"
+						value={this.state.endDate}
+						onChange={(newDate) => {
+							this.setState({...this.state, endDate: newDate});
+						}}
+						renderInput={(params) => <TextField {...params} />}
+					/>
+				</LocalizationProvider>
+				<Button variant="contained" onClick={(event) => { this.fetchHis(event) }} >Get Data</Button>
+			</Stack>
+			// {this.state.isFetching ? <CircularProgress /> : <p>loaded</p>}
+		);
 	}
-
-	return (
-		<Box sx={{ display: "flex", marginTop: 5}}>
-			<Box sx={{ minWidth: 120 }}>
-				<FormControl fullWidth>
-					<InputLabel id="point-select-label">Point</InputLabel>
-					<Select
-						labelId="point-select-label"
-						id="point-select"
-						value={state.point}
-						label="Point"
-						onChange={handlePointSelect}
-					>
-						<MenuItem value={"74BD0182-1346-4813-96AC-757833C2E22E"}>Test</MenuItem>
-						<MenuItem value={"74BD0185-1346-4813-96AC-757833C2E22E"}>FAKE</MenuItem>
-					</Select>
-				</FormControl>
-			</Box>
-			<LocalizationProvider dateAdapter={AdapterDateFns}>
-				<DatePicker
-					label="Start date"
-					value={state.startDate}
-					onChange={(newDate) => {
-						setState({...state, startDate: newDate});
-					}}
-					renderInput={(params) => <TextField {...params} />}
-				/>
-				<DatePicker
-					label="End date"
-					value={state.endDate}
-					onChange={(newDate) => {
-						setState({...state, endDate: newDate});
-					}}
-					renderInput={(params) => <TextField {...params} />}
-				/>
-			</LocalizationProvider>
-			<Button variant="contained" onClick={handleGetData} >Get Data</Button>
-		</Box>
-	);
 }
